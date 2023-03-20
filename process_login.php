24c595e829e9e1a437f46b43a76d3638990cc857
Vitalie Coscodan <v.coscodan@yahoo.com>
2023-03-20 10:27:41 -0500

    removed some debugging

--- process_login.php
+++ process_login.php
@@ -1,6 +1,5 @@
 <html>
     <h1>Login processor</h1>
-<?php echo "Hello!<br>"?>
 <?php
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
@@ -9,15 +8,13 @@ error_reporting(E_ALL);
 <?php $conn = new PDO('mysql:server=localhost;dbname=market', 'market_user', 'market_user');
     if($conn === false) {
         echo "Connection failed<br>"; die();
-    } else {
-        echo "Connection established<br>";
-    }
+    } 
     $email = $_POST['email'];
     $password = $_POST['password'];
     $stmt = $conn->query('select email, password from users where email = \''.$email.'\'');
     $result_set = $stmt->fetch();
     if (empty($result_set)) {
-        echo "Result was empty<br>";
+        echo "Result was empty<br>"; die();
     } else {
         echo "Result was not empty<br>";
     }
@@ -26,6 +23,6 @@ error_reporting(E_ALL);
     } else {
         echo "Failed<br>";
     }
-print_r($_POST); ?>
+?>
 <p>More test</p>
 </html>
